5e0bf896c37d79f7605d449904ec5bb4953507ce
Nicholas Smith <nico.ehs05@gmail.com>
2018-10-23 20:56:47 +0000

    basic purchases

--- app/controllers/producers_controller.rb
+++ app/controllers/producers_controller.rb
@@ -1,2 +1,13 @@
 class ProducersController < ApplicationController
+    
+    def buy
+        @producer=Producer.find_by_id(params[:id])
+        stock=@producer.game.get_stock
+        stock.amount -= (@producer.price)*(params[:amount].to_i)
+        stock.save
+        @producer.amount += (params[:amount].to_i)
+        @producer.save
+        redirect_to game_path(@producer.game)
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :games, only: [:show, :index]
   resources :users, only: [:show]
   root 'games#index'
+  get 'producers/:id/buy/:amount' => 'producers#buy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
