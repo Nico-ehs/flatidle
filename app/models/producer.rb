6a49a4ca6b405e8d5ebd6791ef14254a8e17cbd7
Nicholas Smith <nico.ehs05@gmail.com>
2018-10-25 15:27:09 +0000

    uprade effects on

--- app/models/producer.rb
+++ app/models/producer.rb
@@ -1,11 +1,13 @@
 class Producer < ApplicationRecord
 	belongs_to :game
+	attr_accessor :ajusted_value
+	attr_accessor :ajusted_rate
 	
 	def valid_purchase
 	    game.get_stock.amount >= price
 	end
 
 	def calculate_price
-		price = base_price * (growth_rate ** amount)
+		base_price * (growth_rate ** amount)
 	end
 end
